87e1d8bc177abf542162de0ccb7b5a396aec4460
ChenRongZi <1398881912@qq.com>
2024-08-08 16:31:04 +0800

    08/08/24 commit

--- term1/程序构造和解释/15th_week/proj04/scheme.py
+++ term1/程序构造和解释/15th_week/proj04/scheme.py
@@ -6,12 +6,12 @@ import os
 
 sys.path.append("scheme_reader")
 
-from scheme_classes import *
-from scheme_forms import *
+from scheme_classes    import *
+from scheme_forms      import *
 from scheme_eval_apply import *
-from scheme_builtins import *
-from scheme_reader import *
-from ucb import main, trace
+from scheme_builtins   import *
+from scheme_reader     import *
+from ucb               import main, trace
 
 
 ################
